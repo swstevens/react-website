8949c01c4473c86520dc9af5c2f56ab908209dcf
swstevens <sstevensw@gmail.com>
2023-12-06 18:45:55 -0800

    implement basic square to apply shader to

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,18 +1,48 @@
 import React from "react";
-import Resume from "./Resume";
-
-// import ReactGA from "react-ga";
-// import $ from "jquery";
-import "../App.css";
-// import Header from "./Header";
 // import Resume from "./Resume";
 
+// import "../App.css";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from "@react-three/drei";
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+// const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+// const cube = new THREE.Mesh( geometry, material ); 
+// scene.add( cube );
+// const Cube = () => {
+//   const mesh = useRef();
+//   return (
+//     <mesh ref={mesh}>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshBasicMaterial color={0xffffff} />
+//     </mesh>
+//   );
+// };
 
-export const ProjectsPage = (data) => {
+// export const ProjectsPage = (data) => {
+//   return (
+//     <div className="projectPage">
+//       <Resume data={data.data.resume} />
+//       <Canvas>
+//         <Cube />
+//       </Canvas>
+//     </div>
+//   )
+// }
+function Box () {
   return (
-    <div className="projectPage">
-      <Resume data={data.data.resume} />
-    </div>
+    <mesh>
+      <meshLambertMaterial attach='material' color='hotpink' />
+      <boxGeometry args={[2, 2, 2]} />
+    </mesh>
+  )
+}
+export const ProjectsPage = () => {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <Box />
+    </Canvas>
   )
 }
 
